fd1403bb7c3a6af9c60cb210ad98dfc1806b66d2
João Marcelo <joao_marcelo.22@hotmail.com>
2021-08-11 11:32:46 -0300

    jv

---
+++ exercicios recode/JavaScript/JaScript.js
@@ -0,0 +1,16 @@
+alert("Seja bem vindo");
+
+
+
+
+
+let nome = prompt("Qual o seu nome?");
+
+document.write(nome + ", seja bem vindo ao site!");
+
+
+
+let opcao = confirm("Deseja encerrar?");
+
+ document.write("Sua escolha foi: " + opcao);
+
